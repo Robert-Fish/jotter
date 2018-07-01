0b838bc9b3cac9b8f79e49f4743b330f0774ffb5
Robert <rob@Roberts-Macbook-Pro.local>
2018-07-01 20:55:47 +1000

    initial commit

---
+++ src/Config/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export const DB_CONFIG = {
+  apiKey: "AIzaSyBtVaBLfIX3z-JD4x7UKKxzYBE_2ZuwL8E",
+  authDomain: "jotter-6e51a.firebaseapp.com",
+  databaseURL: "https://jotter-6e51a.firebaseio.com",
+  projectId: "jotter-6e51a",
+  storageBucket: "jotter-6e51a.appspot.com",
+  messagingSenderId: "512474621869"
+};
